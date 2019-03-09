1dc27809919518671e1bc4cb81b24265f2fc99bb
996138852@qq.com <wsyt880908>
2019-03-09 16:09:35 +0800

    Initial commit

---
+++ user-control/src/test/java/com/ytaos/usercontrol/UserControlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ytaos.usercontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
